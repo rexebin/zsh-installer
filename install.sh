fd8a98596f5831835804d0fad1b96566785320c3
Rex <rexebin@gmail.com>
2021-01-04 17:09:21 +0000

    init

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
-sudo apt install zsh
+cp ./.zshrc ~/.zshrc
+cp ./.p10k.zsh ~/.p10k.zsh
 
+sudo apt install zsh
 chsh -s $(which zsh)
 
-cp .zshrc ~/.zshrc
+cd ~
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+rm install.sh
 
-cp .p10k.zsh ~/.p10k.zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-mkdir ~/.zsh
+cd ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/
+git clone https://github.com/zsh-users/zsh-autosuggestions 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 
+git clone https://github.com/supercrabtree/k 
 
-cd ~/.zsh
+git clone git://github.com/wting/autojump.git
+cd autojump
+./install.py
 
-git clone https://github.com/romkatv/powerlevel10k.git
-
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-
-git clone https://github.com/supercrabtree/k.git
-
-git clone https://github.com/wting/autojump.git
