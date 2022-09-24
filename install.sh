64ad43f8b202c18e7e881c71e518fe7f3024b3a4
Rex <rexebin@users.noreply.github.com>
2022-09-24 11:33:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 
 git clone https://github.com/supercrabtree/k 
 
-git clone git://github.com:wting/autojump.git
-cd autojump
-./install.py
-
